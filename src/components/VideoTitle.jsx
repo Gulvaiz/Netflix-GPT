dd71b84589d4b032e71f84c23a1fd6f47b8dd851
Gulvaiz <gulvaizalam12@gmail.com>
2025-04-23 22:37:38 +0530

    asdfasdF

--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -2,7 +2,7 @@
 const VideoTitle = ({title, overview}) => {
   return (
     <>
-        <div className="pt-[13%] px-3 md:px-[10%] absolute text-white bg-gradient-to-r from-black w-screen aspect-video">
+        <div className="pt-[13%] px-3  md:px-[10%] absolute text-white bg-gradient-to-r from-black w-screen aspect-video">
           <div className="mx-2 md:m-0">
             <h1 className="text-2xl md:text-6xl font-bold">{title}</h1>
             <p className="hidden md:inline-block py-6 text-lg w-1/4">{overview}</p>
