d667b81f99b23ad3d06a301c885d73826d2f2209
Gulvaiz <gulvaizalam12@gmail.com>
2025-04-23 23:18:11 +0530

    wertwert

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux"
-import VideoBackground from "../VideoBackground"
-import VideoTitle from "../VideoTitle"
+import VideoBackground from "./VideoBackground"
+import VideoTitle from "./VideoTitle"
 
 const MainContainer = () => {
     
